4cccd1797ea402caaa99683e102bfe3a6948d312
YaelIlagan <yaelilagan@gmail.com>
2026-09-13 18:45:58 +0800

    9/13 up

--- QueuingForm/CashierWindowQueueForm.cs
+++ QueuingForm/CashierWindowQueueForm.cs
@@ -17,18 +17,18 @@ namespace QueuingForm
         {
             InitializeComponent();
 
-            timer1.Tick += new EventHandler(timer1_Tick); //every tick 1000ms will call timer1_Tick method
+            timer1.Tick += new EventHandler(timer1_Tick);
             timer1.Start();
         }
 
-        private void btnRefresh_Click(object sender, EventArgs e)//manual refresh of listBox
+        private void btnRefresh_Click(object sender, EventArgs e)
         {
             DisplayCashierQueue(CashierClass.CashierQueue);
         }
 
         private void btnNext_Click(object sender, EventArgs e)
         {
-            //if may laman yung listBox then dequeue, refresh then display updated queue
+          
             if (CashierClass.CashierQueue.Count > 0)
             {
                 CashierClass.CashierQueue.Dequeue();
@@ -38,16 +38,16 @@ namespace QueuingForm
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            DisplayCashierQueue(CashierClass.CashierQueue); //auto refresh every 1 second
+            DisplayCashierQueue(CashierClass.CashierQueue);
         }
 
-        public void DisplayCashierQueue(IEnumerable CashierList)// foreach for Queue
+        public void DisplayCashierQueue(IEnumerable CashierList)
         {
             lvListCashierQueue.Items.Clear();
 
             foreach (Object obj in CashierList)
             {
-                lvListCashierQueue.Items.Add(obj.ToString());//inserting every number sa queue 1 by 1
+                lvListCashierQueue.Items.Add(obj.ToString());
             }
         }
     }
